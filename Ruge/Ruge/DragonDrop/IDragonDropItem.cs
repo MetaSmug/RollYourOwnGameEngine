6c26e0655fb28c694cb35b77c19ecda411308a75
kati <infinity@soundgoddess.me>
2016-05-18 19:46:23 -0700

    initial commit
    created new repository

---
+++ Ruge/Ruge/DragonDrop/IDragonDropItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.Ruge.DragonDrop {
+    /// <summary>
+    /// Interface describing necessary implementation for working with the DragonDrop Handler.
+    /// </summary>
+    public interface IDragonDropItem {
+        Vector2 Position { get; set; }
+        bool IsSelected { get; set; }
+        bool IsMouseOver { set; }
+        bool Contains(Vector2 pointToCheck);
+        Rectangle Border { get; }
+        bool IsDraggable { get; set; }
+        int ZIndex { get; set; }
+        Texture2D Texture { get; }
+
+        void OnSelected();
+        void OnDeselected();
+        void HandleCollusion(IDragonDropItem item);
+    }
+}
