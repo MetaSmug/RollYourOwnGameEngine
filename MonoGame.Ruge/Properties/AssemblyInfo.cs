fd118b41699cbe6e0fd87a514cbf6c54362c4a34
kati <infinity@soundgoddess.me>
2016-05-23 17:18:15 -0700

    version 0.2 released
    RPG Sprite Sheet Animation

--- MonoGame.Ruge/Properties/AssemblyInfo.cs
+++ MonoGame.Ruge/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("MetaSmug")]
 [assembly: AssemblyProduct("MonoGame.Ruge")]
 [assembly: AssemblyCopyright("Copyright © 2016 MetaSmug.com")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Copyright © 2016 MetaSmug.com")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
